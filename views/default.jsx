e4447b531c78cbfad1c90af87ad4041d6404c132
syossan27 <wisdom1027@gmail.com>
2015-12-17 17:41:23 +0900

    fix test data

--- views/default.jsx
+++ views/default.jsx
@@ -32,7 +32,8 @@ var DefaultLayout = React.createClass({
           <header className="text-center">
             <h1 className="title">They Rejected Us in JP.</h1>
             <div>誰もがお祈りされています。 - これはそんな私達のストーリーです。</div>
-            <div>あなたのストーリーを追加しませんか？, どうぞ <a href="https://github.com/syossan27/rejected-us-jp/issues/new">issueを追加</a> か こちらのTwitterアカウントに連絡 <a href="https://twitter.com/syossan27">@syossan27</a>していただければこちらで追加します。</div>
+            <div>あなたのストーリーを追加しませんか？</div>
+            <div><a href="https://github.com/syossan27/rejected-us-jp/issues/new">issueを追加</a> か こちらのTwitterアカウントに連絡 <a href="https://twitter.com/syossan27">@syossan27</a>していただければこちらで追加します。</div>
           </header>
           <div className="grid text-center Rejections">{this.props.children}</div>
           <script src="/js/masonry.js"></script>
