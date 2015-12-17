ae0668c26f441aac75991ef9d71bb3ef141c3c59
syossan27 <wisdom1027@gmail.com>
2015-12-17 17:37:51 +0900

    translate japanese

--- views/default.jsx
+++ views/default.jsx
@@ -25,14 +25,14 @@ var DefaultLayout = React.createClass({
         </head>
         <body>
           // Github ribbon
-          <a href="https://github.com/jkup/rejected.us">
+          <a href="https://github.com/syossan27/rejected-us-jp">
             <img style={githubStyle} src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></img>
           </a>
           // End Github ribbon
           <header className="text-center">
-            <h1 className="title">They Rejected Us.</h1>
-            <div>Everyone’s been rejected - these are our stories</div>
-            <div>To add your own, please <a href="https://github.com/syossan27/rejected-us-jp/issues/new">open an issue</a> or tweet me at <a href="https://twitter.com/jkup">@jkup</a></div>
+            <h1 className="title">They Rejected Us in JP.</h1>
+            <div>誰もがお祈りされています。 - これはそんな私達のストーリーです。</div>
+            <div>あなたのストーリーを追加しませんか？, どうぞ <a href="https://github.com/syossan27/rejected-us-jp/issues/new">issueを追加</a> か こちらのTwitterアカウントに連絡 <a href="https://twitter.com/syossan27">@syossan27</a>していただければこちらで追加します。</div>
           </header>
           <div className="grid text-center Rejections">{this.props.children}</div>
           <script src="/js/masonry.js"></script>
